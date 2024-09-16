d10a8ccf4adf9653a9fa25bba971d7fae347f979
KYU <uscaio@estudante.ufscar.br>
2024-09-15 21:34:37 -0300

    add: população do bd

--- src/main/java/br/todo_list/TodoListApplication.java
+++ src/main/java/br/todo_list/TodoListApplication.java
@@ -1,7 +1,16 @@
 package br.todo_list;
 
+import br.todo_list.model.TodoItem;
+import br.todo_list.model.TodoList;
+import br.todo_list.model.User;
+import br.todo_list.repository.TodoItemRepository;
+import br.todo_list.repository.TodoListRepository;
+import br.todo_list.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class TodoListApplication {
@@ -10,4 +19,47 @@ public class TodoListApplication {
 		SpringApplication.run(TodoListApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner demo(UserRepository userRepository, TodoListRepository todoListRepository, TodoItemRepository todoItemRepository, PasswordEncoder passwordEncoder) {
+		return (args) -> {
+			// Populando dados de usuários
+			User user1 = new User();
+			user1.setUsername("user1");
+			user1.setPassword(passwordEncoder.encode("password1"));
+			user1.setEmail("user1@example.com");
+			userRepository.save(user1);
+
+			User user2 = new User();
+			user2.setUsername("user2");
+			user2.setPassword(passwordEncoder.encode("password2"));
+			user2.setEmail("user2@example.com");
+			userRepository.save(user2);
+
+			// Populando listas de tarefas
+			TodoList todoList1 = new TodoList();
+			todoList1.setTitle("Groceries");
+			todoList1.setUser(user1);
+			todoListRepository.save(todoList1);
+
+			TodoList todoList2 = new TodoList();
+			todoList2.setTitle("Work Tasks");
+			todoList2.setUser(user1);
+			todoListRepository.save(todoList2);
+
+			// Populando itens de tarefas
+			TodoItem todoItem1 = new TodoItem();
+			todoItem1.setTitle("Buy milk");
+			todoItem1.setDescription("2 liters of milk");
+			todoItem1.setCompleted(false);
+			todoItem1.setTodoList(todoList1);
+			todoItemRepository.save(todoItem1);
+
+			TodoItem todoItem2 = new TodoItem();
+			todoItem2.setTitle("Finish report");
+			todoItem2.setDescription("Complete the annual report");
+			todoItem2.setCompleted(false);
+			todoItem2.setTodoList(todoList2);
+			todoItemRepository.save(todoItem2);
+		};
+	}
 }
