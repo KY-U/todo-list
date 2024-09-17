fc87a62b7f61d99740d88507ae1164b2a5c9808f
KYU <uscaio@estudante.ufscar.br>
2024-09-17 14:53:08 -0300

    testando bd sem security

--- src/main/java/br/todo_list/TodoListApplication.java
+++ src/main/java/br/todo_list/TodoListApplication.java
@@ -12,6 +12,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.Optional;
+
 @SpringBootApplication
 public class TodoListApplication {
 
@@ -19,47 +21,65 @@ public class TodoListApplication {
 		SpringApplication.run(TodoListApplication.class, args);
 	}
 
+	//PasswordEncoder passwordEncoder
 	@Bean
-	public CommandLineRunner demo(UserRepository userRepository, TodoListRepository todoListRepository, TodoItemRepository todoItemRepository, PasswordEncoder passwordEncoder) {
+	public CommandLineRunner demo(UserRepository userRepository, TodoListRepository todoListRepository, TodoItemRepository todoItemRepository) {
 		return (args) -> {
 			// Populando dados de usuários
-			User user1 = new User();
-			user1.setUsername("user1");
-			user1.setPassword(passwordEncoder.encode("password1"));
-			user1.setEmail("user1@example.com");
-			userRepository.save(user1);
+			if (userRepository.findByEmail("user1@example.com").isEmpty()) {
+				User user1 = new User();
+				user1.setUsername("user1");
+				//user1.setPassword(passwordEncoder.encode("password1"));
+				user1.setPassword("password1");
+				user1.setEmail("user1@example.com");
+				userRepository.save(user1);
 
-			User user2 = new User();
-			user2.setUsername("user2");
-			user2.setPassword(passwordEncoder.encode("password2"));
-			user2.setEmail("user2@example.com");
-			userRepository.save(user2);
+				User user2 = new User();
+				user2.setUsername("user2");
+				//user2.setPassword(passwordEncoder.encode("password2"));
+				user1.setPassword("password2");
+				user2.setEmail("user2@example.com");
+				userRepository.save(user2);
+			}
 
-			// Populando listas de tarefas
-			TodoList todoList1 = new TodoList();
-			todoList1.setTitle("Groceries");
-			todoList1.setUser(user1);
-			todoListRepository.save(todoList1);
+			// Verifica se as listas de tarefas já existem
+			if (todoListRepository.findByTitle("Groceries").isEmpty()) {
+				User user1 = userRepository.findByEmail("user1@example.com").orElseThrow();
 
-			TodoList todoList2 = new TodoList();
-			todoList2.setTitle("Work Tasks");
-			todoList2.setUser(user1);
-			todoListRepository.save(todoList2);
+				TodoList todoList1 = new TodoList();
+				todoList1.setTitle("Groceries");
+				todoList1.setUser(user1);
+				todoListRepository.save(todoList1);
 
-			// Populando itens de tarefas
-			TodoItem todoItem1 = new TodoItem();
-			todoItem1.setTitle("Buy milk");
-			todoItem1.setDescription("2 liters of milk");
-			todoItem1.setCompleted(false);
-			todoItem1.setTodoList(todoList1);
-			todoItemRepository.save(todoItem1);
+				TodoList todoList2 = new TodoList();
+				todoList2.setTitle("Work Tasks");
+				todoList2.setUser(user1);
+				todoListRepository.save(todoList2);
+			}
 
-			TodoItem todoItem2 = new TodoItem();
-			todoItem2.setTitle("Finish report");
-			todoItem2.setDescription("Complete the annual report");
-			todoItem2.setCompleted(false);
-			todoItem2.setTodoList(todoList2);
-			todoItemRepository.save(todoItem2);
+			// Verifica se os itens de tarefas já existem
+			if (todoItemRepository.findByTitle("Buy milk").isEmpty()) {
+				TodoList todoList1 = todoListRepository.findByTitle("Groceries").get(0);
+
+				TodoItem todoItem1 = new TodoItem();
+				todoItem1.setTitle("Buy milk");
+				todoItem1.setDescription("2 liters of milk");
+				todoItem1.setCompleted(false);
+				todoItem1.setTodoList(todoList1);
+				todoItemRepository.save(todoItem1);
+
+				TodoItem todoItem2 = new TodoItem();
+				todoItem2.setTitle("Finish report");
+				todoItem2.setDescription("Complete the annual report");
+				todoItem2.setCompleted(false);
+				todoItem2.setTodoList(todoList1);
+				todoItemRepository.save(todoItem2);
+			}
+
+			Optional<User> user = userRepository.findByEmail("user1@example.com");
+			System.out.println("o usuário é:");
+			System.out.println(user.get().getEmail());
+			System.out.println(user.get().getPassword());
 		};
 	}
 }
