eab4358a65212694de17c189da2638cdf1076b1c
KYU <uscaio@estudante.ufscar.br>
2024-09-17 18:42:28 -0300

    add: configração tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.html",
+            "./src/**/*.js",
+            "./src/**/*.jsx",
+            "./src/**/*.tsx",],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
